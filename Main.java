d0f42290c1e03c957d753ad3e92c0eb5ecb4c521
AustinKavelman <kavea9410@wrdsb.ca>
2020-09-17 23:12:29 +0000

    Finished program that determines which number tonight.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program determines which number is greater
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //Gets the users first interger and prints it on the screen
+    System.out.println("Please enter the first interger:");
+    int a = input.nextInt();
     
+    //Gets the users second interger and prints it on the screen
+    System.out.println("Please enter the second interger:");
+    int b = input.nextInt();
+
+    //Checks to see which number is larger
+    if (a > b) { 
+    System.out.println("The first number is larger");
+    } else if (b > a) {
+    System.out.println("The second number is larger");
+    } else if (a == b) {
+    System.out.println("Both of the intergers are equal");
+    }
+
+
   }
 }
